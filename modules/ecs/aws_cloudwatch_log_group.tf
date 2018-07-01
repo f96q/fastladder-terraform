6355849d9f5d26480f60e9936c23bc4de34e65bd
danny <danny.dev8@gmail.com>
2018-07-02 01:04:16 +0900

    Fix log retention

--- modules/ecs/aws_cloudwatch_log_group.tf
+++ modules/ecs/aws_cloudwatch_log_group.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_log_group" "fastladder_rails" {
   name = "${var.fastladder}-rails"
-  retention_in_days = 7
+  retention_in_days = 1
 }
 
 resource "aws_cloudwatch_log_group" "fastladder_mysql" {
   name = "${var.fastladder}-mysql"
-  retention_in_days = 7
+  retention_in_days = 1
 }
